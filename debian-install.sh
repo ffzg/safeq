58b72fac0003c4522abf1ab435a380eb99e4eb6e
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-03-02 08:49:23 +0100

    run through sudo, fix snmp mib enable

--- debian-install.sh
+++ debian-install.sh
@@ -1,4 +1,8 @@
-sudo apt-get install cups printer-driver-fujixerox cups-ipp-utils printer-driver-cups-pdf snmp snmp-mibs-downloader
+#!/bin/sh
+
+test `id -u` -ne 0 && exec sudo $0
+
+apt-get install cups printer-driver-fujixerox cups-ipp-utils printer-driver-cups-pdf snmp snmp-mibs-downloader
 
 # enable mibs
-grep -i '^mibs :' /etc/snmp/snmp.conf && perl -p -i -n -e 's/^mibs : .*/mibs +ALL/' /etc/snmp/snmp.conf
+grep -i '^mibs :' /etc/snmp/snmp.conf && perl -p -i -n -e 's/^mibs :.*/mibs +ALL/' /etc/snmp/snmp.conf
