8035fa1d860082c2dddb1790eef5584540c1e90e
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-03-04 10:01:07 +0000

    add curl and Data::Dump

--- debian-install.sh
+++ debian-install.sh
@@ -2,7 +2,7 @@
 
 test `id -u` -ne 0 && exec sudo $0
 
-apt-get install cups printer-driver-fujixerox cups-ipp-utils printer-driver-cups-pdf snmp snmp-mibs-downloader
+apt-get install curl libdata-dump-perl cups printer-driver-fujixerox cups-ipp-utils printer-driver-cups-pdf snmp snmp-mibs-downloader
 
 # enable mibs
 grep -i '^mibs :' /etc/snmp/snmp.conf && perl -p -i -n -e 's/^mibs :.*/mibs +ALL/' /etc/snmp/snmp.conf
